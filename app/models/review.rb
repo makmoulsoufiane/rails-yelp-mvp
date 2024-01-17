a28e862a6f16407b6892fd9009d09f1dba70c677
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-01-17 15:16:27 +0100

    yelp help

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Review model represents reviews for restaurants.
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+end
